83f38768973178b974f8d7ca54cc96b0583d9691
mguggiari <micaela.guggiari@davinci.edu.ar>
2023-11-30 17:07:14 -0300

    update editar

--- proyecto-dv/source/pages/CursoEditar.vue
+++ proyecto-dv/source/pages/CursoEditar.vue
@@ -1,51 +1,60 @@
-<script>
+<script setup>
 import PrimaryButton from '../components/PrimaryButton.vue';
-import Loader from '../components/Loader.vue';
+import LoadingContext from '../components/LoadingContext.vue';
 import PrimaryInput from '../components/PrimaryInput.vue';
 import PrimaryTextarea from '../components/PrimaryTextarea.vue';
-import { getProductoById, editarProducto } from '../services/productos.js'
+import { useRoute, useRouter } from 'vue-router';
+import { ref, onMounted, inject } from 'vue';
+import { getProductoById, editarProducto } from '../services/productos.js';
+import { notificacionSymbol } from "../symbols/symbols";
 
-export default {
-    name: "CursoEditar",
-    components: { PrimaryButton, Loader, PrimaryTextarea, PrimaryInput },
-    data() {
-        return {
-            productoCargando: true,
-            form: {
-                titulo: '',
-                duracion: '',
-                precio: '',
-                descripcion: '',
-            },
-        };
-    },
-    methods: {
-        async handleEditarProducto() {
-            this.producto.titulo = this.form.titulo;
-            this.producto.duracion = this.form.duracion;
-            this.producto.precio = this.form.precio;
-            this.producto.descripcion = this.form.descripcion;
+const route = useRoute();
+const router = useRouter();
+const { setNotificacion } = inject(notificacionSymbol);
 
-            await editarProducto(this.$route.params.id, this.producto);
+const producto = ref({});
+const productoCargando = ref(true);
 
-            this.$router.push('/panel-admin');
-        },
-    },
-    async mounted() {
-        this.productoCargando = true;
-        this.producto = await getProductoById(this.$route.params.id);
-        this.productoCargando = false;
+const form = ref({
+    titulo: '',
+    duracion: '',
+    precio: '',
+    descripcion: '',
+});
 
-        this.form.titulo = this.producto.titulo;
-        this.form.duracion = this.producto.duracion;
-        this.form.precio = this.producto.precio;
-        this.form.descripcion = this.producto.descripcion;
-    },
+onMounted(async () => {
+    try {
+        productoCargando.value = true;
+        const productData = await getProductoById(route.params.id);
+
+        producto.value = productData;
+        form.value = productData;
+    } catch (error) {
+        console.error(error);
+    } finally {
+        productoCargando.value = false;
+    }
+});
+
+const handleEditarProducto = async () => {
+    try {
+        await editarProducto(route.params.id, form.value);
+        router.push('/panel-productos');
+        setNotificacion({
+            mensaje: 'Curso actualizado con éxito',
+            type: 'success',
+        });
+    } catch (error) {
+        setNotificacion({
+            mensaje: 'Error al actualizar el curso, aguarde unos segundos y vuelva a intentar, por favor',
+            type: 'error',
+        });
+    }
 };
 </script>
+
 <template>
-    <Loader v-if="productoCargando" />
-    <template v-else>
+    <LoadingContext :loading="productoCargando">
         <div class="flex justify-center items-center mt-16">
             <div class="bg-white p-8 rounded-md shadow-md md:w-2/4">
                 <h1 class="mb-6 text-3xl font-bold leading-tight text-blue-950 md:text-4xl lg:text-5xl">Editar</h1>
@@ -76,5 +85,5 @@ export default {
                 </form>
             </div>
         </div>
-    </template>
+    </LoadingContext>
 </template>
